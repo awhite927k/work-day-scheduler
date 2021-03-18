ed2acf103973632ebf50dbc26c92f430ff997d7d
Alex White <awhite214@gmail.com>
2021-03-18 19:14:29 -0400

    updating HTML and CSS files

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    const startWeek = moment().startOf('month').week();
+    const endWeek = moment().endOf('month').week();
+    
+    
+    let calendar = []
+    for(var week = startWeek; week<endWeek;week++){
+      calendar.push({
+        week:week,
+        days:Array(7).fill(0).map((n, i) => moment().week(week).startOf('week').clone().add(n + i, 'day'))
+      })
+    }
+});
+
