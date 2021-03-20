d9006b8cc4e1b4c9fca5da655ea41f0c73d223b1
Alex White <awhite214@gmail.com>
2021-03-20 18:19:03 -0400

    adding color coding function to js file

--- Assets/script.js
+++ Assets/script.js
@@ -8,5 +8,21 @@ var $thisBlock = $(this);
 $currentDay.text(currentDate);
 
 // code for saving entries locally
-// code for color-coding the times
 
+//code for color-coding the time blocks 
+function timeColors() {
+    var thisBlockHr = parseInt($thisBlock.attr("cal-hour"));
+        if (thisBlockHr < currentHour) {
+            $thisBlock.addClass("past").removeClass("present future");
+        }
+        else if (thisBlockHr == currentHour) {
+            $thisBlock.addClass("present").removeClass("past future");
+        }
+        else {
+            $thisBlock.addClass("future").removeClass("present past");
+        }
+    }
+
+$(document).ready(function () {
+    timeColors();
+});
